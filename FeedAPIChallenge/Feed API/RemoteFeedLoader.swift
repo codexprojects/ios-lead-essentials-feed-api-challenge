78759a15591e6f51f2c651f7a11f8f2d1710c520
Ilke Yucel <ilke.yucel@morhipo.com>
2021-08-26 17:52:35 +0300

    Delivers Invalid data error on 200 HTTP response with invalid json

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,10 +21,14 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url, completion: { result in
 			switch result {
-			case let .success((_, response)):
+			case let .success((data, response)):
 				guard response.statusCode == 200 else {
 					completion(.failure(Error.invalidData))
 					return
+				}
+
+				if let _ = try? JSONSerialization.jsonObject(with: data) {} else {
+					completion(.failure(Error.invalidData))
 				}
 			default:
 				completion(.failure(Error.connectivity))
